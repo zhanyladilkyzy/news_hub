6fcc3f7284ea2b6f8200bc175b5c29c5cc59a069
zhanyladilkyzy <52228972+zhanyladilkyzy@users.noreply.github.com>
2020-07-05 14:15:06 +0600

    release v2

--- core/models.py
+++ core/models.py
@@ -1,13 +1,56 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 
 # Create your models here.
 
-class Users(models.Model):
-    name = models.CharField(max_length=200)
-    lastname = models.CharField(max_length=200)
-    username = models.CharField(max_length=200)
-    password = models.CharField(max_length=200)
+class MainUserManager(BaseUserManager):
+    """
+        Main user manager
+        Methods
+        -------
+        create_user(self, username, password=None, full_name=None, email=None)
+            creates user with params
+    """
+
+    def create_user(self, username, password=None, full_name=None, email=None):
+        """
+        Creates user
+        :param username: username of user
+        :type username: str
+        :return: instance of user
+        Attributes
+        ----------
+        username: str
+            username of user
+        """
+        if not username:
+            raise ValueError('User must have a username')
+        user = self.model(username=username, email=email, full_name=full_name)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, password):
+        """
+        Creates and saves a superuser with the given email and password
+        """
+        user = self.model(username=username)
+        user.set_password(password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
+
+
+class MainUser(AbstractBaseUser, PermissionsMixin):
+    """ Repersent a "user profile" inside our system """
+    fullname = models.CharField(max_length=200)
+    username = models.CharField(max_length=200, unique=True)
+    is_staff = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'username'
+
+    objects = MainUserManager()
 
 
 class Category(models.Model):
@@ -16,11 +59,8 @@ class Category(models.Model):
 
 class News(models.Model):
     title = models.CharField(max_length=200)
-    author =  models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
     text = models.CharField(max_length=500)
-    #image
-    #comments = models.CharField(max_length=500)
-    #likes = models.CharField(max_length=500)
     publishing_data = models.DateTimeField()
 
     def __str__(self):
@@ -28,15 +68,11 @@ class News(models.Model):
 
 
 class Comments(models.Model):
-    news_id = models.ForeignKey(News, on_delete=models.CASCADE,
-                                related_name='news_comment')
-    user_id = models.ForeignKey(Users, on_delete=models.CASCADE,
-                                related_name='user_comment')
+    news = models.ForeignKey(News, on_delete=models.CASCADE)
+    user = models.ForeignKey(MainUser, on_delete=models.CASCADE)
     feedback = models.CharField(max_length=500)
 
 class Likes(models.Model):
-    news_id = models.ForeignKey(News, on_delete=models.CASCADE,
-                                related_name='news_like')
-    user_id = models.ForeignKey(News, on_delete=models.CASCADE,
-                                related_name='user_like')
-    liked = models.CharField(max_length=10, blank=False, null=False)
+    news = models.ForeignKey(News, on_delete=models.CASCADE)
+    user = models.ForeignKey(MainUser, on_delete=models.CASCADE)
+    liked = models.BooleanField(default=False)
